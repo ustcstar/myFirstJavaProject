0a85e744912f6c4b4c5d5e5b326dfe47e2aed9c0
ustcstar <465707858@qq.com>
2016-03-28 10:01:11 +0800

    Initial commit

---
+++ src/helloWorld.java
@@ -0,0 +1,8 @@
+/**
+ * Created by oukinsei on 16/3/27.
+ */
+public class helloWorld {
+    public static void main(String[] args){
+        System.out.println("hello,world!This is my first java programe on IDEA!");
+    }
+}
